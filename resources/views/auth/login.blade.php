3dd120391b8708ff35e682b589edad9a7e3502fe
Jewel Rana <jewelrana.dev@gmail.com>
2021-12-13 22:23:19 +0600

    bug fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,32 +38,6 @@
                 </div>
                 <button type="submit" class="btn btn-primary btn-block" tabindex="4">Sign in</button>
             </form>
-
-            <p class="text-center mt-2">
-                <span>New on our platform?</span>
-                <a href="{{ route('register') }}">
-                    <span>Create an account</span>
-                </a>
-            </p>
-
-{{--            <div class="divider my-2">--}}
-{{--                <div class="divider-text">or</div>--}}
-{{--            </div>--}}
-
-{{--            <div class="auth-footer-btn d-flex justify-content-center">--}}
-{{--                <a href="javascript:void(0)" class="btn btn-facebook">--}}
-{{--                    <i data-feather="facebook"></i>--}}
-{{--                </a>--}}
-{{--                <a href="javascript:void(0)" class="btn btn-twitter white">--}}
-{{--                    <i data-feather="twitter"></i>--}}
-{{--                </a>--}}
-{{--                <a href="javascript:void(0)" class="btn btn-google">--}}
-{{--                    <i data-feather="mail"></i>--}}
-{{--                </a>--}}
-{{--                <a href="javascript:void(0)" class="btn btn-github">--}}
-{{--                    <i data-feather="github"></i>--}}
-{{--                </a>--}}
-{{--            </div>--}}
         </div>
     </div>
     <!-- /Login v1 -->
